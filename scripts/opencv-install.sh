6d7a528c0825784a63b3cab968b563152117e569
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-05-09 06:38:01 +0000

    cleanup install script

--- scripts/opencv-install.sh
+++ scripts/opencv-install.sh
@@ -1,51 +1,39 @@
 #!/bin/bash
 
 OPENCV_VERSION="4.11.0"
+OPENCV_INSTALL_PATH="/opt/opencv-${OPENCV_VERSION}"
 
-wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
-
-# python paths
-# Find Python 3 paths (run these commands and use their output)
-PYTHON3_EXECUTABLE=$(which python3)
-PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-PYTHON3_LIBRARY=$(python3 -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR') + '/' + sysconfig.get_config_var('LDVERSION') + sysconfig.get_config_var('ABIFLAGS') + sysconfig.get_config_var('EXT_SUFFIX'))" | sed 's/\.so.*/\.so/')
-PYTHON3_PACKAGES_PATH=$(python3 -c "import site; print(site.getusersitepackages())") # Or site.getsitepackages()[0] if installing as root
-
-echo "PYTHON3_EXECUTABLE: $PYTHON3_EXECUTABLE"
-echo "PYTHON3_INCLUDE_DIR: $PYTHON3_INCLUDE_DIR"
-echo "PYTHON3_LIBRARY: $PYTHON3_LIBRARY"
-echo "PYTHON3_PACKAGES_PATH: $PYTHON3_PACKAGES_PATH"
-
-### install openvino
-
-OPENVINO_INSTALL_DIR="/opt/intel/openvino_2025"
-
-wget -O openvino.tgz https://storage.openvinotoolkit.org/repositories/openvino/packages/2025.1/linux/openvino_toolkit_ubuntu24_2025.1.0.18503.6fec06580ab_x86_64.tgz
-tar -xvf openvino.tgz
-sudo mkdir -p ${OPENEVINO_INSTALL_DIR}
-sudo mv openvino_toolkit_ubuntu24_2025.1.0.18503.6fec06580ab_x86_64 ${OPENEVINO_INSTALL_DIR}}
-cd ${OPENEVINO_INSTALL_DIR}
-sudo -E ./install_dependencies/install_openvino_dependencies.sh
-source ${OPENEVINO_INSTALL_DIR}/setupvars.sh
-
-### end openvino install
-
-### install onnx
+OPENVINO_INSTALL_PATH="/opt/intel/openvino_2025"
+OPENVINO_VERSION="2025.1"
+OPENVINO_TAG="2025.1.0.18503.6fec06580ab_x86_64"
 
 # ONNX_VERSION="gpu-1.12.0" # for gpu
 ONNX_VERSION="1.12.0"
 ONNX_ROOT_DIR="/opt/onnxruntime"
 
+### install openvino
+
+wget -O openvino.tgz https://storage.openvinotoolkit.org/repositories/openvino/packages/${OPENVINO_VERSION}/linux/openvino_toolkit_ubuntu24_${OPENVINO_TAG}.tgz
+tar -xvf openvino.tgz
+sudo mkdir -p ${OPENEVINO_INSTALL_DIR}
+sudo mv openvino_toolkit_ubuntu24_${OPENVINO_TAG}/* ${OPENEVINO_INSTALL_DIR}
+rm -rf openvino_toolkit_ubuntu24_${OPENVINO_TAG} openvino.tgz
+cd ${OPENEVINO_INSTALL_DIR}
+sudo -E ${OPENVINO_INSTALL_PATH}/install_dependencies/install_openvino_dependencies.sh
+source ${OPENEVINO_INSTALL_DIR}/setupvars.sh
+
+### end openvino install
+
+### install onnx runtime
+
 wget -O onnx.tgz "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-x64-${ONNX_VERSION}.tgz"
 tar -xvf onnx.tgz
-sudo mv "onnxruntime-linux-x64-${ONNX_VERSION}" ${ONNX_ROOT_DIR}
+sudo mv "onnxruntime-linux-x64-${ONNX_VERSION}/*" ${ONNX_ROOT_DIR}
+rm -rf onnxruntime-linux-x64-${ONNX_VERSION} onnx.tgz
 
 ### end onnx install
 
-# other dependencies
+### install other dependencies
 
 sudo apt install -y \
     ninja-build \
@@ -65,7 +53,9 @@ sudo apt install -y \
     libtbb-dev \
     libprotobuf-dev \
     python3-dev \
-    python3-numpy
+    python3-numpy \
+    ffmpeg \
+    intel-opencl-icd \
 
 # lapack path issue https://github.com/opencv/opencv/issues/12957
 # sudo ln -s /usr/include/lapacke.h /usr/include/x86_64-linux-gnu
@@ -75,11 +65,31 @@ if [ -f /usr/include/x86_64-linux-gnu/cblas.h ]; then
     sudo ln -s /usr/include/x86_64-linux-gnu/cblas.h /usr/include/cblas.h
 fi
 
-OPENCV_INSTALL_PATH="/opt/opencv-${OPENCV_VERSION}"
+### end install other dependencies
+
+### install opencv
+
+# fetch opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+# python paths
+# Find Python 3 paths (run these commands and use their output)
+PYTHON3_EXECUTABLE=$(which python3)
+PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON3_LIBRARY=$(python3 -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR') + '/' + sysconfig.get_config_var('LDVERSION') + sysconfig.get_config_var('ABIFLAGS') + sysconfig.get_config_var('EXT_SUFFIX'))" | sed 's/\.so.*/\.so/')
+PYTHON3_PACKAGES_PATH=$(python3 -c "import site; print(site.getusersitepackages())") # Or site.getsitepackages()[0] if installing as root
+
+echo "PYTHON3_EXECUTABLE: $PYTHON3_EXECUTABLE"
+echo "PYTHON3_INCLUDE_DIR: $PYTHON3_INCLUDE_DIR"
+echo "PYTHON3_LIBRARY: $PYTHON3_LIBRARY"
+echo "PYTHON3_PACKAGES_PATH: $PYTHON3_PACKAGES_PATH"
 
 mkdir -p build && cd build
 
-### Configure build
+# configure build
 
 # Find CUDA arch (compute capability) for your specific GPU(s)
 # Look up your GPU model online or use nvidia-smi to find its compute capability
@@ -125,15 +135,19 @@ cmake \
     -DWITH_LAPACK=ON \
     -DWITH_GTK=ON \
     -DBUILD_NEW_PYTHON_SUPPORT=ON \
-    -DCMAKE_PREFIX_PATH="${ONNX_ROOT_DIR};${OPENVINO_INSTALL_DIR};/usr" \
+    -DCMAKE_PREFIX_PATH="${ONNX_ROOT_DIR};${OPENVINO_INSTALL_PATH};/usr" \
     -DBUILD_opencv_gapi=OFF \
     ../opencv-${OPENCV_VERSION}
 
-### Build and install
+# Build and install
 ninja -j$(nproc)
 sudo ninja install
 
-### Add to shellrc file
+# cleanup
+cd .. # back from build
+rm -rf opencv-${OPENCV_VERSION} opencv_contrib-${OPENCV_VERSION} build opencv.zip opencv_contrib.zip
+
+# Add to shellrc file
 
 # check shell and add to shell file, only zsh and bash supported
 if [ "$(basename "$0")" = "zsh" ]; then
@@ -143,5 +157,5 @@ elif [ "$(basename "$0")" = "bash" ]; then
 fi
 
 echo "export LD_LIBRARY_PATH=${OPENCV_INSTALL_PATH}/lib:\$LD_LIBRARY_PATH" >> "$SHELLRC"
-echo "source ${OPENVINO_INSTALL_DIR}/setupvars.sh" >> "$SHELLRC"
+echo "source ${OPENVINO_INSTALL_PATH}/setupvars.sh" >> "$SHELLRC"
 source $SHELLRC
